a00c1536e22a5adbe02ad0e6ffd5a16f32b6d8ff
Yashavanthagouda <yashavantha@cdac.in>
2024-07-11 15:41:23 +0530

    DSA

--- DSA.py
+++ DSA.py
@@ -6,51 +6,51 @@ print(twoDArray)
 newTwoDArry=np.delete(twoDArray,0 ,axis=1)
 print(newTwoDArry)
 
-# def searchTwoDArray(array,value):
-#     for i in range (len(array)):
-#         for j in range (len(array[0])):
-#             if array[i][j]==value:
-#                 return "the value is located at: "+str(i)+" " +str(j)
+def searchTwoDArray(array,value):
+    for i in range (len(array)):
+        for j in range (len(array[0])):
+            if array[i][j]==value:
+                return "the value is located at: "+str(i)+" " +str(j)
             
-#     return "value not found"
+    return "value not found"
 
-# print(searchTwoDArray(twoDArray,255))        
+print(searchTwoDArray(twoDArray,255))        
 
-# def searchTwoDArray(array, value):
-#     for i in range(len(array)):
-#         for j in range(len(array[i])):
-#             if array[i][j] == value:
-#                 return f"The value is located at: {i} {j}"
-#     return "Value not found"
+def searchTwoDArray(array, value):
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            if array[i][j] == value:
+                return f"The value is located at: {i} {j}"
+    return "Value not found"
 
-# def createTwoDArray():
-#     rows = int(input("Enter the number of rows: "))
-#     cols = int(input("Enter the number of columns: "))
-#     array = []
+def createTwoDArray():
+    rows = int(input("Enter the number of rows: "))
+    cols = int(input("Enter the number of columns: "))
+    array = []
     
-#     for i in range(rows):
-#         row = []
-#         print(f"Enter values for row {i + 1}, separated by spaces:")
-#         row_values = input().split()
-#         if len(row_values) != cols:
-#             print(f"Error: You must enter exactly {cols} values.")
-#             return createTwoDArray()
-#         for value in row_values:
-#             row.append(int(value))
-#         array.append(row)
+    for i in range(rows):
+        row = []
+        print(f"Enter values for row {i + 1}, separated by spaces:")
+        row_values = input().split()
+        if len(row_values) != cols:
+            print(f"Error: You must enter exactly {cols} values.")
+            return createTwoDArray()
+        for value in row_values:
+            row.append(int(value))
+        array.append(row)
     
-#     return array
+    return array
 
-# # Get user input for the 2D array
-# print("Create your 2D array:")
-# twoDArray = createTwoDArray()
-# print("Your 2D Array:")
-# for array in twoDArray:
-#     print(array)
+# Get user input for the 2D array
+print("Create your 2D array:")
+twoDArray = createTwoDArray()
+print("Your 2D Array:")
+for array in twoDArray:
+    print(array)
 
-# # Get user input for the value to search
-# search_value = int(input("Enter the value to search for: "))
+# Get user input for the value to search
+search_value = int(input("Enter the value to search for: "))
 
-# # Search for the value in the 2D array
-# print(searchTwoDArray(twoDArray, search_value))
+# Search for the value in the 2D array
+print(searchTwoDArray(twoDArray, search_value))
 
--- list.py
+++ list.py
@@ -1,141 +1,139 @@
- # a LIST is a data structure that holds an ordered collection of items
-# ex:
-# integer_list=[1,2,3]
-# string_list=['yash','a','patil']
-# list=[1,2,0.35,'sir',[78,'ram',778.34]]
+## a LIST is a data structure that holds an ordered collection of items
+#ex:
+integer_list=[1,2,3]
+string_list=['yash','a','patil']
+list=[1,2,0.35,'sir',[78,'ram',778.34]]
 
-## Creating list
-# integer_list=[1,2,3]
-# print(integer_list)
+# Creating list
+integer_list=[1,2,3]
+print(integer_list)
 
-# string_list=['yash','a','patil']
-# print(string_list)
+string_list=['yash','a','patil']
+print(string_list)
 
-# list=[1,2,0.35,'sir',[78,'ram',778.34]]
-# print(list)
+list=[1,2,0.35,'sir',[78,'ram',778.34]]
+print(list)
 
-# empty_list=[]
-# print(empty_list)
+empty_list=[]
+print(empty_list)
 
-##
 #Accessing list/Traversing
-# shoping_list=['milk','soap','dress']
-# print(shoping_list[0])
-# print(shoping_list[-2])
-# print('milk' in shoping_list)
+shoping_list=['milk','soap','dress']
+print(shoping_list[0])
+print(shoping_list[-2])
+print('milk' in shoping_list)
 
-# print("++++++++")
-# for i in shoping_list:
-#     print(i)
+print("++++++++")
+for i in shoping_list:
+    print(i)
 
-# print("++++++++")
-# for i in range (len(shoping_list)):
-#     shoping_list[i]=shoping_list[i]+ " " +'XXXX'
-#     print(shoping_list[i])
+print("++++++++")
+for i in range (len(shoping_list)):
+    shoping_list[i]=shoping_list[i]+ " " +'XXXX'
+    print(shoping_list[i])
 
-# print("++++++++")
-# empty=[]
-# print("kjb")
-# for i in empty:
-#     print("i am empty")
-#     print ("liksbg")
- ##
+print("++++++++")
+empty=[]
+print("kjb")
+for i in empty:
+    print("i am empty")
+    print ("liksbg")
+ 
 
 #Update/insert list
 
-# myList=[1,2,3,4,5,6,7,8]
-# print(myList)
-# # myList[2]=33
-# # myList[4]=55
-# # print(myList)
+myList=[1,2,3,4,5,6,7,8]
+print(myList)
+myList[2]=33
+myList[4]=55
+print(myList)
 
-# #insert method:inserting the element at any given index
-# # myList.insert(0,11)
-# # print(myList)
-# # myList.insert(4,14)
-# # print(myList)
+#insert method:inserting the element at any given index
+myList.insert(0,11)
+print(myList)
+myList.insert(4,14)
+print(myList)
 
-# #Appand method:insert the element at the end of the list
-# # myList.append(99)
-# # print(myList)
+#Appand method:insert the element at the end of the list
+myList.append(99)
+print(myList)
 
-# #Extend method:Adding the new list to the existing list
-# newList=[11,12,13,14]
-# myList.extend(newList)
-# print(myList)
+#Extend method:Adding the new list to the existing list
+newList=[11,12,13,14]
+myList.extend(newList)
+print(myList)
 
-##Delete/Slice
+#Delete/Slice
 
-# myList=[1,2,3,4,5,6,7,8]
-# print(myList)
-# myList[0:2]=['x','y']
-# print(myList[0:])
+myList=[1,2,3,4,5,6,7,8]
+print(myList)
+myList[0:2]=['x','y']
+print(myList[0:])
 
-#POP method:delete the element through the index or
-#      if you are not providede the index it will delete the last element
-# print(myList.pop())  
-# print(myList)
+##POP method:delete the element through the index or if you are not providede the index it will delete the last element
+print(myList.pop())  
+print(myList)
 
-#DELETE method:delete the element through the index
-# del myList[:-1]
-# print(myList)
+## DELETE method:delete the element through the index
+del myList[:-1]
+print(myList)
 
-##Remove method:it will delete/remove the direct providede value
+#Remove method:it will delete/remove the direct providede value
 
-# myList.remove(1)
-# print(myList)
+myList.remove(1)
+print(myList)
 
-## SEARCHING element in the list
-# my_list=[10,20,30,40,50,60,70,80,90]
+# SEARCHING element in the list
+my_list=[10,20,30,40,50,60,70,80,90]
 
-# target=50
+target=50
 
-## IN OPERATOR
-# if target in my_list:
-#     print(f"{target} is in the list")
-# else:
-#     print(f"{target} is not in the list")    
+# IN OPERATOR
+if target in my_list:
+    print(f"{target} is in the list")
+else:
+    print(f"{target} is not in the list")    
 
-## LINEAR SEARCH
+# LINEAR SEARCH
 
-# def linear_search(p_list,p_target):
-#     for i,value in enumerate(p_list):
-#         if value==p_target:
-#             return i
-#     return -1
+def linear_search(p_list,p_target):
+    for i,value in enumerate(p_list):
+        if value==p_target:
+            return i
+    return -1
 
-# print(linear_search(my_list,target))    
+print(linear_search(my_list,target))    
 
-## LIST OPERATIOR ?FUNCTION
+# LIST OPERATIOR ?FUNCTION
 
-# a=[1,2,3] 
-# b=[4,5,6,22]
-# # c=a+b ## '+' Operator 
+a=[1,2,3] 
+b=[4,5,6,22]
+# c=a+b ## '+' Operator 
 
-# a=[1,4]
+a=[1,4]
 # a=a*5  '*' Operator
-## len():Return the number of elements inside list
-# a=[1,2,3,4,5,5,6,7,78,8] 
-# print(len(a))
-## MAX():returns the item with highest value in the list 
-# print(max(a))
-## MIN():returns the item with lowest value in the list 
-# print(min(a))
-##SUM():return the sum of the items in the list
-# print(sum(a))
-# print(sum(a)/len(a))
+# len():Return the number of elements inside list
+a=[1,2,3,4,5,5,6,7,78,8] 
+print(len(a))
+# MAX():returns the item with highest value in the list 
+print(max(a))
+# MIN():returns the item with lowest value in the list 
+print(min(a))
+#SUM():return the sum of the items in the list
+print(sum(a))
+print(sum(a)/len(a))
 
-# my_list=list()
-# while(True):
-#     inp = input('enter a number: ')
-#     if inp == 'done':break
-#     value=float(inp)
-#     my_list.append(value)
-# avg=sum(my_list)/len(my_list)
+my_list=list()
+while(True):
+    inp = input('enter a number: ')
+    if inp == 'done':break
+    value=float(inp)
+    my_list.append(value)
+avg=sum(my_list)/len(my_list)
 
-# print('AVARAGE: ',avg)
+print('AVARAGE: ',avg)
 
- ## CONVERTING STRING TO LIST 
+ # CONVERTING STRING TO LIST 
 
 str = 'spam'
 my_list=list(str)
@@ -144,3 +142,40 @@ print(my_list)
 str = 'spam list spam'
 my_list =str.split()
 print(my_list)
+
+str = 'spam-list-spam'
+delimiter='a'
+b=str.split(delimiter)
+print(b)
+print(delimiter.join(b))
+
+# LIST COMPREHENSION
+prev_list=[1,2,3]
+new_list=[i*2 for i in prev_list]
+print(prev_list)
+print(new_list)
+
+lan='python'
+new_list=[letter for letter in lan]
+print(lan)
+print(new_list)
+
+# CONDITIONAL LIST COMPREHENSION
+prev_list=[-1,10,-20,-3,30,40]
+new_list=[number*number for number in prev_list if number<0]
+print(new_list)
+
+sentence='my name is yashu patil'
+
+def is_consonant(letter):
+    vowels='aeiou'
+    return letter.isalpha() and letter.lower() not in vowels
+
+# print(is_consonant('y'))
+
+consonants=[i for i in sentence if is_consonant(i)]
+print(consonants)
+
+prev_list=[-1,10,-3,40,50,-2,38,-78,-28]
+new_list=[number if number>0 else '-ve number' for number in prev_list]
+print(new_list)
